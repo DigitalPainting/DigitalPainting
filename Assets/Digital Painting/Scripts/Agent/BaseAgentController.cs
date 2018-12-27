d05a176dfc58037f0df26512103bb8bc3b067e2e
Ross Gardler <rgardler@apache.org>
2018-12-26 23:45:07 -0800

    add basic FlyByWire camera drone

--- Assets/Digital Painting/Scripts/Agent/BaseAgentController.cs
+++ Assets/Digital Painting/Scripts/Agent/BaseAgentController.cs
@@ -8,28 +8,29 @@ namespace wizardscode.agent
     {
         [Header("Movement")]
         [Tooltip("Walking speed under normal circumstances")]
-        public float NormalMovementSpeed = 6;
+        public float normalMovementSpeed = 1;
         [Tooltip("The factor by which to multiply the walking speed when moving fast.")]
-        public float FastMovementFactor = 4;
+        public float fastMovementFactor = 4;
         [Tooltip("The factor by which to multiply the walking speed when moving slowly.")]
-        public float SlowMovementFactor = 0.2f;
+        public float slowMovementFactor = 0.2f;
         [Tooltip("Speed at which the agent will climb/drop in flight. Set to 0 if you don't want them to fly.")]
-        public float ClimbSpeed = 4;
+        public float climbSpeed = 1;
         [Tooltip("Speed at which the agent will rotate.")]
-        public float RotationSpeed = 90;
+        public float rotationSpeed = 90;
 
         [Header("Controls")]
-        public float rotationSensitivity = 100;
+        [Tooltip("Mouse look sensitivity.")]
+        public float mouseLookSensitivity = 100;
 
         float rotationX = 0;
         float rotationY = 0;
 
 
-        void Update()
+        internal virtual void Update()
         {
             // Look with the mouse
-            rotationX += Input.GetAxis("Mouse X") * rotationSensitivity * Time.deltaTime;
-            rotationY += Input.GetAxis("Mouse Y") * rotationSensitivity * Time.deltaTime;
+            rotationX += Input.GetAxis("Mouse X") * mouseLookSensitivity * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * mouseLookSensitivity * Time.deltaTime;
             rotationY = Mathf.Clamp(rotationY, -90, 90);
             transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
             transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
@@ -37,28 +38,28 @@ namespace wizardscode.agent
             // Move with the keyboard controls 
             if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
             {
-                transform.position += transform.forward * (NormalMovementSpeed * FastMovementFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-                transform.position += transform.right * (NormalMovementSpeed * FastMovementFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+                transform.position += transform.forward * (normalMovementSpeed * fastMovementFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+                transform.position += transform.right * (normalMovementSpeed * fastMovementFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
             }
             else if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
             {
-                transform.position += transform.forward * (NormalMovementSpeed * SlowMovementFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
-                transform.position += transform.right * (NormalMovementSpeed * SlowMovementFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
+                transform.position += transform.forward * (normalMovementSpeed * slowMovementFactor) * Input.GetAxis("Vertical") * Time.deltaTime;
+                transform.position += transform.right * (normalMovementSpeed * slowMovementFactor) * Input.GetAxis("Horizontal") * Time.deltaTime;
             }
             else
             {
-                transform.position += transform.forward * NormalMovementSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-                transform.position += transform.right * NormalMovementSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
+                transform.position += transform.forward * normalMovementSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
+                transform.position += transform.right * normalMovementSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
             }
 
             if (Input.GetKey(KeyCode.Q))
             {
-                transform.position += transform.up * ClimbSpeed * Time.deltaTime;
+                transform.position += transform.up * climbSpeed * Time.deltaTime;
             }
 
             if (Input.GetKey(KeyCode.E))
             {
-                transform.position -= transform.up * ClimbSpeed * Time.deltaTime;
+                transform.position -= transform.up * climbSpeed * Time.deltaTime;
             }
         }
     }
---
+++ Assets/Digital Painting/Scripts/Agent/CameraDroneController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wizardscode.agent
+{
+    public class CameraDroneController : BaseAgentController
+    {
+        [Header("Camera Drone Controls")]
+        [Tooltip("Allow manual control to override automated control?")]
+        public bool allowManual = true;
+
+        internal override void Update()
+        {
+            Vector3 position = transform.position;
+            Quaternion rotation = transform.rotation;
+            if (allowManual)
+            {
+                base.Update();
+            }
+
+            if (position == transform.position && rotation == transform.rotation)
+            {
+                FlyByWire();
+            }
+        }
+
+        /// <summary>
+        /// Moves the drone according to the fly by wire program being carried.
+        /// </summary>
+        private void FlyByWire()
+        {
+            transform.position += transform.forward * normalMovementSpeed * Time.deltaTime;
+        }
+    }
+}
