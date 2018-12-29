c1dc4a88547bb7cbb157d50ed1943ea437512985
Ross Gardler <rgardler@apache.org>
2018-12-29 03:05:48 -0800

    Add a very simple Day Night script

---
+++ Assets/Digital Painting/Docs/DayNightCycle.md
@@ -0,0 +1,27 @@
+﻿# Day Night Cycle
+
+It is intended that the Digital Painting will use other assets to deliver a day night cycle. The idea is that users select an implementation that delivers the right atmosphere for their scene. However, it's obviously not possible to provide automatic support for other assets. Someone needs to integrate the solution first and even before that we need a way to abstract the settings needed by different assets. So, at the current time we provide a simple Day Night Solution and plan to add support for more in the future. Patches welcome.
+
+## Supported Day/Night Assets
+
+At present The Digital Painting supports the following assets available:
+
+  * Very Simple Day Night Cycle (Built In - See `Environment/VerySimpleDayNightCycle` script)
+
+## Adding A Day Night Cycle to Your Digital Painting
+
+
+### Very Simple Day Night Cycle (Built In)
+
+This is about as simple as a day night cycle can get - and it's pretty ugly too. The Sun rises and sets at exactly the same time at exactly the simple place. We will gladly accept pull requests to improve this, but in reality we expect most users to use a specialist Asset.
+
+  * Create an empty object called `Managers`
+  * Drop the `Environment/DayNightCycle` script onto the `Managers` object
+  * Add a directional light (if you don't have one already) and rename it to Sun.
+
+## Possible Future Supported Assets
+
+Patches are welcome to add support for any Day Night Cycle solution. Here are a few that might be interesting.
+
+  * Spyblood Games [Simple Day And Night Cycle System](https://assetstore.unity.com/packages/templates/tutorials/simple-day-and-night-cycle-system-66647) - Free from the Unity Asset Store
+  * Flat Tutorials [Day and Night Cycle with Scattering and Moving Clouds](https://assetstore.unity.com/packages/tools/particles-effects/day-and-night-cycle-with-scattering-and-moving-clouds-27024) - Free on the Unity Asset Store
---
+++ Assets/Digital Painting/Scripts/Environment/VerySimpleDayNightCycle.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+
+public class VerySimpleDayNightCycle : MonoBehaviour {
+    [Header("Time of Day")]
+    [Tooltip("Current time of day, in seconds.")]
+    [Range(0, 86400)]
+    public float currentTimeOfDay = 5 * 60 * 60; // (5 AM)
+    [Tooltip("The speed at which a game day passes.")]
+    public int secondsPerDay = 96;
+
+    [Header("Sun settings")]
+    [Tooltip("How bright should the sun be at mid day?")]
+    public float maxIntensity = 1.1f;
+
+    [Header("Overrides")]
+    [Tooltip("The directional light that acts as the sun. If blank a light with the name `Sun` will be used.")]
+    public Light sun;    
+
+    private void Start()
+    {
+        if (sun == null)
+        {
+            InitializeSun();
+        }
+    }
+
+    private void InitializeSun()
+    {
+        GameObject go = GameObject.Find("Sun");
+        if (go == null)
+        {
+            go = new GameObject("Sun");
+            sun = go.AddComponent<Light>();
+            sun.type = LightType.Directional;
+            sun.shadows = LightShadows.Soft;
+        }
+        else
+        {
+            sun = go.GetComponent<Light>();
+        }
+    }
+
+    private void Update()
+    {
+        UpdateTime();
+        UpdateSunPosition();
+        UpdateSunIntensity();
+    }
+
+    public void UpdateTime()
+    {
+        currentTimeOfDay += Time.deltaTime * (86400 / secondsPerDay);
+        if (currentTimeOfDay > 86400)
+        {
+            currentTimeOfDay = 0;
+        }
+    }
+
+    private void UpdateSunIntensity()
+    {
+        float intensity;
+        if (currentTimeOfDay < 43200)
+        {
+            intensity = 1 - (43200 - currentTimeOfDay) / 43200;
+        }
+        else
+        {
+            intensity = 1 - ((43200 - currentTimeOfDay) / 43200 * -1);
+        }
+        sun.intensity = intensity * maxIntensity;
+    }
+
+    private void UpdateSunPosition()
+    {
+        sun.transform.rotation = Quaternion.Euler(new Vector3((currentTimeOfDay - 21600) / 864000 * 360, 0, 0));
+    }
+}
