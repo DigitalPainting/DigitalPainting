4c1f3190f565869eef7d495b4f8ff5f3d0fe6957
Ross Gardler <rgardler@apache.org>
2018-12-30 12:17:27 -0800

    Better tooltip for barriers. Add AI Barriers prefab as an example

--- Assets/Digital Painting/Scripts/Agent/AIAgentController.cs
+++ Assets/Digital Painting/Scripts/Agent/AIAgentController.cs
@@ -32,7 +32,7 @@ namespace wizardscode.digitalpainting.agent
         public float maxAngleOfRandomPathChange = 25;
 
         [Header("Overrides")]
-        [Tooltip("Collider within which the agent must stay. If null a default object will be searched for using the name `" + DEFAULT_BARRIERS_NAME + "`.")]
+        [Tooltip("Set of objects within which the agent must stay. Each object must have a collider and non-kinematic rigid body. If null a default object will be searched for using the name `" + DEFAULT_BARRIERS_NAME + "`.")]
         public GameObject barriers;
 
         private const string DEFAULT_BARRIERS_NAME = "AI Barriers";
