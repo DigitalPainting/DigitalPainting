bb48b83e4475c461eb1d834579a9cd1f181cbe1c
Ross Gardler <rgardler@apache.org>
2018-12-31 02:54:59 -0800

    Add DevTestUI to other Demo scene and to WeatherMaker test scene

--- Assets/DigitalPaintinIntegrations/WeatherMaker/Scripts/WeatherMakerDayNightCycle.cs
+++ Assets/DigitalPaintinIntegrations/WeatherMaker/Scripts/WeatherMakerDayNightCycle.cs
@@ -13,24 +13,21 @@ namespace wizardscode.environment.WeatherMaker
         [Header("Weather Maker Day Night")]
         [Tooltip("The Weather Maker Profile to use")]
         public WeatherMakerDayNightCycleProfileScript weatherMakerProfile;
-
-        private WeatherMakerDayNightCycleManagerScript manager;
-
+        
         internal override float GetTime()
         {
-            return manager.TimeOfDay;
+            return WeatherMakerDayNightCycleManagerScript.Instance.TimeOfDay;
         }
 
         internal override void Initialize(float startTime)
         {
             base.Initialize(startTime);
 
-            manager = WeatherMakerDayNightCycleManagerScript.Instance;
-            if (manager == null)
+            if (WeatherMakerDayNightCycleManagerScript.Instance == null)
             {
                 Debug.LogError("Cannot find the Weather Maker Manager Script, please place the `WeatherMakerPrefab` into your scene. See the `DigitalPaintingIntegrations/README.md` for more details.");
             }
-            manager.DayNightProfile = weatherMakerProfile;
+            WeatherMakerDayNightCycleManagerScript.Instance.DayNightProfile = weatherMakerProfile;
         }
 
         internal override void InitializeCamera()
@@ -61,20 +58,20 @@ namespace wizardscode.environment.WeatherMaker
 
         internal override void InitializeTiming()
         {
-            manager.TimeOfDay = startTime;
+            WeatherMakerDayNightCycleManagerScript.Instance.TimeOfDay = startTime;
             float daySpeed = 1440 / dayCycleInMinutes;
             float nightSpeed = daySpeed;
 
             weatherMakerProfile.Speed = daySpeed;
             weatherMakerProfile.NightSpeed = daySpeed;
 
-            manager.DayNightProfile = weatherMakerProfile;
-            manager.DayNightProfile.UpdateFromProfile(true);
+            WeatherMakerDayNightCycleManagerScript.Instance.DayNightProfile = weatherMakerProfile;
+            WeatherMakerDayNightCycleManagerScript.Instance.DayNightProfile.UpdateFromProfile(true);
         }
 
         internal override void SetTime(float timeInSeconds)
         {
-            manager.TimeOfDay = timeInSeconds;
+            WeatherMakerDayNightCycleManagerScript.Instance.TimeOfDay = timeInSeconds;
         }
 
         internal override void Update()
