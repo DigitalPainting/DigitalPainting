5a0c66ba1a07ca481a852ed39c2be8f345951980
Ross Gardler <ross@gardler.org>
2019-01-01 22:07:50 -0800

    provide access to the agent with focus

--- Assets/Digital Painting/Scripts/DigitalPaintingManager.cs
+++ Assets/Digital Painting/Scripts/DigitalPaintingManager.cs
@@ -12,13 +12,46 @@ namespace wizardscode.digitalpainting
         [Tooltip("The Agent prefab to use as the primary character - that is the one the camera will follow.")]
         public BaseAgentController agentPrefab;
 
+        private Cinemachine.CinemachineClearShot _clearshot;
+
+        private BaseAgentController _agent;
+        /// <summary>
+        /// Get or set the agent that has the current focus of the camera.
+        /// </summary>
+        public BaseAgentController AgentWithFocus {
+            get { return _agent; }
+            set
+            {
+                _agent = value;
+                _clearshot.Follow = _agent.transform;
+                _clearshot.LookAt = _agent.transform;
+            }
+        }
+
         // Use this for initialization
         void Start()
         {
+            CreateCamera();
+            AgentWithFocus = CreateAgent();
+        }
+
+        /// <summary>
+        /// Create the default camera rig.
+        /// </summary>
+        private void CreateCamera()
+        {
+            _clearshot = GameObject.Instantiate(cameraRigPrefab);
+        }
+
+
+        /// <summary>
+        /// Create the main agent that the cameras will follow initially.
+        /// </summary>
+        /// <returns></returns>
+        private BaseAgentController CreateAgent()
+        {
             GameObject agent = GameObject.Instantiate(agentPrefab).gameObject;
-            Cinemachine.CinemachineClearShot clearshot = GameObject.Instantiate(cameraRigPrefab);
-            clearshot.Follow = agent.transform;
-            clearshot.LookAt = agent.transform;
+            return agent.GetComponent<BaseAgentController>();
         }
     }
 }
