c8a0df4238c0612b0dfb621bbae2c15256e70bbb
Ross Gardler <rgardler@apache.org>
2019-01-07 22:25:40 -0800

    add precipitation type dropdown and keep UI in sync with current settings

--- Assets/Digital Painting/Scripts/DevTest/Environment/WeatherUI.cs
+++ Assets/Digital Painting/Scripts/DevTest/Environment/WeatherUI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,7 +8,9 @@ namespace wizardscode.environment.test
 {
     public class WeatherUI : MonoBehaviour
     {
-        [Tooltip("Dropdown for coosing precipitation type")]
+        [Tooltip("Toggle to enable.disable auto update of the weather.")]
+        public Toggle isAuto;
+        [Tooltip("Dropdown for choosing precipitation type.")]
         public Dropdown precipitationTypeDropdown;
 
         WeatherManager manager;
@@ -20,8 +23,18 @@ namespace wizardscode.environment.test
             precipitationTypeDropdown.ClearOptions();
 
             List<Dropdown.OptionData> options = new List<Dropdown.OptionData>();
-            options.Add(new Dropdown.OptionData())
+            Array types = Enum.GetValues(typeof(AbstractWeatherSystem.PrecipitationType));
+            foreach (AbstractWeatherSystem.PrecipitationType type in types)
+            {
+                options.Add(new Dropdown.OptionData(type.ToString()));
+            }
             precipitationTypeDropdown.AddOptions(options);
+        }
+
+        private void Update()
+        {
+            isAuto.isOn = manager.implementation.AutomaticUpdates;
+            precipitationTypeDropdown.value = (int)manager.implementation.precipitationType;
         }
 
         public void OnPrecipitationIntensityChanged(float newValue)
@@ -32,6 +45,11 @@ namespace wizardscode.environment.test
         public void OnIsAutoEnabledChanged(bool newValue)
         {
             manager.implementation.AutomaticUpdates = newValue;
+        }
+
+        public void OnPrecipitationTypeChanged(int typeIndex)
+        {
+            manager.implementation.precipitationType = (AbstractWeatherSystem.PrecipitationType)Enum.GetValues(typeof(AbstractWeatherSystem.PrecipitationType)).GetValue(typeIndex);
         }
     }
 }
