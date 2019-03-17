c46177866f4a8fd55e079ee2aff5f3383a0e9033
Ross Gardler <rgardler@apache.org>
2019-03-16 23:12:43 -0700

    Make Spawnable Objects definitions ScriptableObjects. Allow SimpleSpawner to spawn more than one kind of object. Add spawning of bushes to the demo scene. Rename scripta/data folder to ScriptableObjects folder.

--- Assets/Digital Painting/Scripts/Environment/SimpleSpawner.cs
+++ Assets/Digital Painting/Scripts/Environment/SimpleSpawner.cs
@@ -5,35 +5,24 @@ using UnityEngine;
 namespace wizardscode.environment {
     public class SimpleSpawner : MonoBehaviour
     {
-        [Tooltip("Prefab to spawn.")]
-        public GameObject prefab;
-        [Tooltip("Position of the center point of the spawn area.")]
-        public Vector3 center = Vector3.zero;
-        [Tooltip("Radius within which to spawn the objects.")]
-        public float radius = 10f;
-        [Tooltip("Is the item to be spawned at ground level?")]
-        public bool isGrounded = true;
-        [Tooltip("Number to be spawned.")]
-        public int number = 10;
-        [Tooltip("Additional Y offset. This can be used to do things like ensure the trunk of a tree completely penetrates the ground.")]
-        public float yOffset = 0;
-        [Tooltip("Minimum size. All objects spawned will be a size of (1, 1, 1) this value.")]
-        public float minSize = 1;
+        public SpawnableObject[] objects;
                
         private void Start()
         {
-            Vector3 realCenter = center;
-            realCenter.y = Terrain.activeTerrain.SampleHeight(center);
-            for (int i = 0; i < number; i++)
-            {
-                float size = Random.Range(minSize, 1);
-                Vector3 pos = (Random.insideUnitSphere * radius) + realCenter;
-                if (isGrounded)
+            for (int i = 0; i < objects.Length; i++) {
+                Vector3 realCenter = objects[i].center;
+                realCenter.y = Terrain.activeTerrain.SampleHeight(objects[i].center);
+                for (int c = 0; c < objects[i].number; c++)
                 {
-                    pos.y = Terrain.activeTerrain.SampleHeight(pos) + (yOffset * size);
+                    float size = Random.Range(objects[i].minSize, 1);
+                    Vector3 pos = (Random.insideUnitSphere * objects[i].radius) + realCenter;
+                    if (objects[i].isGrounded)
+                    {
+                        pos.y = Terrain.activeTerrain.SampleHeight(pos) + (objects[i].yOffset * size);
+                    }
+                    GameObject obj = Instantiate(objects[i].prefab, pos, Quaternion.identity);
+                    obj.transform.localScale = new Vector3(size, size, size);
                 }
-                GameObject obj = Instantiate(prefab, pos, Quaternion.identity);
-                obj.transform.localScale = new Vector3(size, size, size);
             }
         }
     }
---
+++ Assets/Digital Painting/Scripts/ScriptableObjects/SpawnableObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wizardscode.environment
+{
+    [CreateAssetMenu(fileName = "Spawnable Object", menuName = "Wizards Code/Environment/Spawnable Object")]
+    public class SpawnableObject : ScriptableObject
+    {
+        [Tooltip("Prefab to spawn.")]
+        public GameObject prefab;
+        [Tooltip("Position of the center point of the spawn area.")]
+        public Vector3 center = Vector3.zero;
+        [Tooltip("Radius within which to spawn the objects.")]
+        public float radius = 10f;
+        [Tooltip("Is the item to be spawned at ground level?")]
+        public bool isGrounded = true;
+        [Tooltip("Number to be spawned.")]
+        public int number = 10;
+        [Tooltip("Additional Y offset. This can be used to do things like ensure the trunk of a tree completely penetrates the ground.")]
+        public float yOffset = 0;
+        [Tooltip("Minimum size. All objects spawned will be a size of (1, 1, 1) this value.")]
+        public float minSize = 1;
+    }
+}
