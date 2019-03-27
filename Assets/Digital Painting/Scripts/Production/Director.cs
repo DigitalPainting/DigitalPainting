0ef0e0f6f28617c6eac694ed066cdd3da6197058
Ross Gardler <rgardler@apache.org>
2019-03-26 18:02:11 -0700

    Add ability to use a prefab for the ClearShot camera. A lower priority trigger to prevent the Thing specific camera's being overridden by the catch all cameras.

--- Assets/Digital Painting/Scripts/Production/Director.cs
+++ Assets/Digital Painting/Scripts/Production/Director.cs
@@ -14,7 +14,7 @@ namespace wizardscode.production
     {
         [SerializeField] [Tooltip("The SO Variable Reference indicating which agent currently has focus.")]
         private BaseAgentControllerReference _agentWithFocus;
-        [SerializeField] [Tooltip("Main Cinemachine camera rig. If this is left as a null then a Default follow camera setup will be created.")]
+        [SerializeField] [Tooltip("Main Cinemachine camera rig. This can either be a rig in the scene, a prefab that will be instantiated or null. In the case of a null a Default follow camera setup will be created.")]
         private CinemachineVirtualCameraBase defaultCameraRig;
 
         private CameraReference _mainCameraReference;
@@ -50,15 +50,14 @@ namespace wizardscode.production
         {
             if (defaultCameraRig == null)
             {
-                GameObject go = new GameObject("Default follow ClearShot");
-                defaultCameraRig = go.AddComponent<CinemachineClearShot>();
-                defaultCameraRig.m_Priority = 100;
-
-                go = new GameObject("Default follow Virtual Camera");
-                go.transform.SetParent(defaultCameraRig.transform);
-                CinemachineVirtualCamera vcam = go.AddComponent<CinemachineVirtualCamera>();
-                vcam.AddCinemachineComponent<CinemachineTransposer>();
-                vcam.AddCinemachineComponent<CinemachineComposer>();
+                CreateDefaultClearShot();
+            } else
+            {
+                if (!defaultCameraRig.gameObject.scene.IsValid())
+                {
+                    CinemachineVirtualCameraBase rig = Instantiate(defaultCameraRig);
+                    rig.gameObject.name = "Default Cinemachine ClearShot Camera";
+                }
             }
 
             Camera camera = Camera.main;
@@ -80,6 +79,27 @@ namespace wizardscode.production
                 Debug.LogWarning("Camera did not have an Flare Layer, adding one. You should probably add one to your camera in the scene.");
                 camera.gameObject.AddComponent<FlareLayer>();
             }
+        }
+
+        private void CreateDefaultClearShot()
+        {
+            GameObject go = new GameObject("Default follow ClearShot");
+            defaultCameraRig = go.AddComponent<CinemachineClearShot>();
+            defaultCameraRig.m_Priority = 100;
+            go.AddComponent<CinemachineCollider>();
+
+            go = new GameObject("Default follow Virtual Camera");
+            go.transform.SetParent(defaultCameraRig.transform);
+            CinemachineVirtualCamera vcam = go.AddComponent<CinemachineVirtualCamera>();
+
+            CinemachineFramingTransposer transposer = vcam.AddCinemachineComponent<CinemachineFramingTransposer>();
+            transposer.m_DeadZoneWidth = 0.3f;
+            transposer.m_DeadZoneHeight = 0.35f;
+            transposer.m_DeadZoneDepth = 3f;
+
+            CinemachineComposer composer = vcam.AddCinemachineComponent<CinemachineComposer>();
+            composer.m_DeadZoneWidth = 0.5f;
+            composer.m_DeadZoneWidth = 0.5f;
         }
     }
 }
